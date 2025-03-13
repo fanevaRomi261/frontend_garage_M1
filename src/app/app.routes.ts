149a8bbea151ffdfc40f2abf78f58c443366b545
fanevaRomi261 <118768031+fanevaRomi261@users.noreply.github.com>
2025-03-13 10:10:31 +0300

    init vehicule client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { LayoutMainComponent } from './layout/layout-main/layout-main.component';
 import { AccueilComponent } from './pages/accueil/accueil.component';
+import { VehiculeClientComponent } from './pages/vehicule-client/vehicule-client.component';
 
 export const routes: Routes = [
   // { path: 'layout' , component: LayoutMainComponent},
@@ -12,6 +13,7 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'accueil', component: AccueilComponent },
+      { path: 'vehicule-client', component: VehiculeClientComponent },
     ],
   },
   { path: 'login', component: LoginComponent },
---
+++ src/app/pages/vehicule-client/vehicule-client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicule-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicule-client.component.html',
+  styleUrl: './vehicule-client.component.css'
+})
+export class VehiculeClientComponent {
+
+}

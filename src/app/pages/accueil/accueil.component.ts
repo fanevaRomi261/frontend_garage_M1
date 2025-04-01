ed458c69d1681bce3c7a825dd5845df7682300f4
Ismael-Raveloson <120553853+Ismael-Raveloson@users.noreply.github.com>
2025-04-02 00:48:40 +0300

    correction accueil

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
 export class AccueilComponent {
   public planningRouter = "/planning";
+
+  userRole!: string;
+
+  ngOnInit() : void{
+    this.loadUserRoleAndPermissions();
+  }
+
+  loadUserRoleAndPermissions(): void {
+    const userData = localStorage.getItem('userData');
+    if (userData) {
+      const infoUser = JSON.parse(userData);
+      this.userRole = infoUser.profil_id.libelle.toLowerCase();
+    }
+  }
+
+
+
 }
